f2c52b3c0bd478aea27e30066e37df4c2b8bd425
certhakzu <pealveco@yahoo.com>
2022-06-09 03:27:13 -0500

    Creacion del la estructura del proyecto

---
+++ src/main/java/com/gestion/productos/model/Producto.java
@@ -0,0 +1,19 @@
+package com.gestion.productos.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Producto {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nombre;
+    private float precio;
+
+
+}
